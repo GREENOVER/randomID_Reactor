fcedad009d69c66698694b663f2ed9684cc38e43
chanwoo <humains29@gmail.com>
2021-07-07 12:28:50 +0900

    feat: Auto change text interval to 3seconds

--- randomID_Reactor/View/RandomIDViewController.swift
+++ randomID_Reactor/View/RandomIDViewController.swift
@@ -6,20 +6,24 @@ class RandomIDViewController: UIViewController, StoryboardView {
   var disposeBag = DisposeBag()
   let reactor = RandomIDReactor()
   
+  let count = Observable<Int>
+    .interval(RxTimeInterval.seconds(3), scheduler: ConcurrentDispatchQueueScheduler(qos: .userInteractive))
+  
   @IBOutlet weak var idLabel: UILabel!
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var changeButton: UIButton!
   
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(true)
-    bind(reactor: reactor)
-    reactor.action.onNext(.viewDidAppear)
-  }
-  
   func bind(reactor: RandomIDReactor) {
+    reactor.action.onNext(.viewDidAppear)
+    
+    count
+      .map { _ in Reactor.Action.viewDidAppear }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
+    
     self.changeButton.rx.tap
       .map { .clickButton }
-      .bind(to:reactor.action)
+      .bind(to: reactor.action)
       .disposed(by: self.disposeBag)
     
     reactor.state.map(\.displayIDText)
